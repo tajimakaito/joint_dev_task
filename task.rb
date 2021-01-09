ec12f841799b135c27ad736ce0e03a4c6db912e9
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-01-09 09:02:13 +0900

    finish ruby basic task Q4

--- task.rb
+++ task.rb
@@ -5,6 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names << "斎藤"
+  puts names
 
 end
 
@@ -13,6 +15,8 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
+  array = array1 + array2
+  puts array
 
 end
 
@@ -20,6 +24,7 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
 
 end
 
@@ -27,6 +32,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
 
   # 以下は変更しないで下さい
   p sports
@@ -37,7 +43,11 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts array1.size == 0
+  if array2.size >= 1
+    puts "false"
+  end
+  
 end
 
 def q6
