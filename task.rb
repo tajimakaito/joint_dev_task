6b4cf697b026530edac786a697087555c1b3705c
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-01-10 20:36:11 +0900

    Ruby 課題基礎 課題11終了

--- task.rb
+++ task.rb
@@ -89,7 +89,7 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    food.include?("うに") ? (puts "好物です") : (puts "まぁまぁ好きです")
+    puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
   end
 end
 
@@ -97,6 +97,11 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports2 = sports.flatten.uniq
+  puts "ユーザーの趣味一覧"
+  sports2.each.with_index(1) { |sport, i|
+  puts "No#{i} #{sport}"
+  }
 
 end
 
