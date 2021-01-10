6b68af672406b51b2bfc02c89849269e912d7d4a
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-01-10 20:59:28 +0900

    Ruby 課題基礎 課題11　レビューでの指摘事項の修正

--- task.rb
+++ task.rb
@@ -97,9 +97,9 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports2 = sports.flatten.uniq
+  sports.flatten!.uniq!
   puts "ユーザーの趣味一覧"
-  sports2.each.with_index(1) { |sport, i|
+  sports.each.with_index(1) { |sport, i|
   puts "No#{i} #{sport}"
   }
 
